0c072acb823b4a586dae28c62b7fc24a30a7f534
NamJunHyone <njh0602@naver.com>
2016-02-20 18:50:52 +0900

    inventory folder changed

---
+++ Classes/UserInterface/Inventory/Inventory.cpp
@@ -0,0 +1,17 @@
+//
+//  Inventory.cpp
+//  TheDeadForest-v2
+//
+//  Created by mac on 2016. 2. 20..
+//
+//
+
+#include "Inventory.hpp"
+
+namespace realtrick
+{
+    namespace userinterface
+    {
+        
+    }
+}
